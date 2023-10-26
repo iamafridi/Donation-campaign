ed41f0d1752fe79d39986222edff83a9db6ba067
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-10-26 23:56:40 +0600

    added card and loaded details

--- src/components/DonationCards/DonationCard.jsx
+++ src/components/DonationCards/DonationCard.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
-const DonationCard = () => {
+const DonationCard = ({ donation }) => {
+
+
+    const { title, category, color, amount, description, image_url } = donation || {}
+
+
+
+
+
+
+
     return (
-        <div>
-            Habibi
+        <div className='py-5'>
+            <div className=" flex flex-col text-gray-700 bg-white shadow-md rounded-xl w-80 bg-clip-border">
+                <div className=" mx-4 -mt-6 overflow-hidden text-white  rounded-xl bg-blue-gray-500 bg-clip-border shadow-blue-gray-500/40">
+                    <img
+                        src={donation.image_url}
+                        alt="img-blur-shadow"
+                        layout="fill"
+                    />
+                </div>
+                 <div className="p-6">
+                 <div className="badge badge-primary badge-outline justify-start">{donation.category}</div>
+                    <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                        {donation.title}
+                    </h5>
+
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/DonationCards/DonationCards.jsx
+++ src/components/DonationCards/DonationCards.jsx
@@ -6,9 +6,9 @@ const DonationCards = ({donations}) => {
         <div className="py-10">
             <h2 className="text-2xl text-center">Donations Here</h2>
 
-<div>
+<div className=" grid justify-center items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-10 ">
     {
-        donations ?.map(donation=><DonationCard key={donation.color}></DonationCard>)
+        donations ?.map(donation=><DonationCard key={donation.color} donation={donation}></DonationCard>)
     }
 </div>
            
