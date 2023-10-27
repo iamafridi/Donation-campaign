bdb3ba5c4b9d4facf76cdcd1f3cf39ae4899508b
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-10-27 20:57:43 +0600

    total donation ammoun added

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -10,11 +10,17 @@ const Donation = () => {
 
     const [isShow, setIsShow] = useState(false)
 
+    const [totalDonation,setTotlaDonation] = useState(0)
 
     useEffect(() => {
         const donationItems = JSON.parse(localStorage.getItem('Donations'));
         if (donationItems) {
             setDonations(donationItems)
+
+            const total = donationItems.reduce((preValue,currentItem)=> preValue + currentItem.amount,0)
+
+            console.log(total);
+            setTotlaDonation(total)
         }
         else {
             setNoFound("NO DATA FOUND")
@@ -34,7 +40,10 @@ const Donation = () => {
             {noFound ? <p className='h-[80vh] flex justify-center items-center'>{noFound}</p>
                 :
                 <div>
-                    {donations.length > 0 && <button onClick={handleRemove} className='btn btn-primary block mx-auto' >Delete All Donation</button>}
+                    <div className='flex  items-center  gap-5 p-5'>
+                        <h1 className='text-2xl font-bold'>Total Donation : {totalDonation}</h1>
+                        {donations.length > 0 && <button onClick={handleRemove} className='btn btn-primary block mx-auto justify-end mr-10' >Delete All Donation</button>}
+                    </div>
 
                     <div className='grid grid-cols-2 gap-5 items-center justify-center p-2'>
                         {
